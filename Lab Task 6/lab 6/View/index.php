f2648345973dc21ad98a69c24c1ff237d4b80c8c
Tajimul1113 <73891325+Tajimul1113@users.noreply.github.com>
2021-12-13 00:18:56 +0600

    Add files via upload

---
+++ Lab Task 6/lab 6/View/index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Welcome to FundBD</title>
+  <link rel="shortcut icon" href="../assets/icon.png">
+  <style>
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+li {
+  float: left;
+}
+
+li a, .dropbtn {
+  display: inline-block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover, .dropdown:hover .dropbtn {
+  background-color: red;
+}
+
+li.dropdown {
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1;}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+</style>
+</head>
+<body>
+  <style>  
+  body {
+background:   grey; url("../assets/icon.png") no-repeat fixed center; 
+}
+</style>
+  <!-- ...............................Header Start....................... -->
+  
+    <fieldset>
+      <table align="Left">
+        <tr>
+          <td>
+            <img src="../assets/imgpsh_fullsize.png" alt="" srcset=""width="200" height="80">
+          </td>
+          
+        </tr>
+      </table>
+      <table align="center"   >
+        <tr>
+          <td>
+            <ul>
+  <li><a href="./index.php">Home</a></li>
+  <li><a href="./loginType.php">Login</a></li>
+  <li><a href=" ./regType.php./regType.php">Registration</a></li>
+  <li><a href=" ./aboutUS.php">About Us</a></li>
+   
+          </td>
+        </tr>
+      </table>
+    </fieldset>
+    <!-- .................................Header Close..................... -->
+
+
+    
+
+    <!-- .................................Main Content..................... -->
+    <fieldset>
+      <h1>
+        Welcome To FundBD <br><br>
+       Here To Help Mankind <br><br>
+        
+
+      </h1>
+      
+        
+      
+        <br><br>
+    </fieldset>
+    <!-- .................................Main Content..................... -->
+    <!-- .................................Footer Start..................... -->
+    <fieldset>
+      <p align="right">  copyright &#169;2021</p>
+    </fieldset>
+
+    <!-- .................................Footer Close..................... -->
+</body>
+</html>
